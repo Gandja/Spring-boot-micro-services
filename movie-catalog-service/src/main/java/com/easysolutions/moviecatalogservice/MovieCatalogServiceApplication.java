6804a0d9db0abc38dd5128f7d4ede35e351f8361
denisgandza <gandja.denis@yandex.ru>
2020-10-08 13:08:22 +0300

    Change RestTemplate to WebClient.Builder

--- movie-catalog-service/src/main/java/com/easysolutions/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/easysolutions/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,7 +3,9 @@ package com.easysolutions.moviecatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -11,6 +13,11 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 	public static void main(String[] args) {
