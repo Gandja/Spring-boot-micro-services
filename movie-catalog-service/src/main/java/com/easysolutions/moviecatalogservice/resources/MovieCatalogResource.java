2d20c34dabb3b6f5c404e1f1f5a9080843339650
denisgandza <gandja.denis@yandex.ru>
2020-10-08 13:42:24 +0300

    Maked services communication

--- movie-catalog-service/src/main/java/com/easysolutions/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/easysolutions/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.easysolutions.moviecatalogservice.resources;
 import com.easysolutions.moviecatalogservice.models.Catalog;
 import com.easysolutions.moviecatalogservice.models.Movie;
 import com.easysolutions.moviecatalogservice.models.Rating;
+import com.easysolutions.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,19 +28,16 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<Catalog> getCatalog(@PathVariable("userId") String userId) {
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1", 5),
-                new Rating("2", 7)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/rating/users/" + userId, UserRating.class);
 
-        return ratings.stream().map(rating -> {
-            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
+        return ratings.getUserRating().stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+//            Movie movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    .block();
 
             return new Catalog(movie.getName(), "Description", rating.getRating());
         }).collect(Collectors.toList());
--- ratings-data-service/src/main/java/com/easysolutions/ratingsdataservice/resources/RatingDataResource.java
+++ ratings-data-service/src/main/java/com/easysolutions/ratingsdataservice/resources/RatingDataResource.java
@@ -1,17 +1,34 @@
 package com.easysolutions.ratingsdataservice.resources;
 
 import com.easysolutions.ratingsdataservice.models.Rating;
+import com.easysolutions.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rating")
 public class RatingDataResource {
 
     @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId){
+    public Rating getRating(@PathVariable("movieId") String movieId) {
 
         return new Rating(movieId, 9);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1", 5),
+                new Rating("2", 7)
+        );
+
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+
+        return userRating;
     }
 }
